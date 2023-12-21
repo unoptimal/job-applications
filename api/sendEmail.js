38dbd2a2954ff7c9a9bf9780c05b564d7ce068b4
unoptimal <unoptimalblogs@gmail.com>
2023-12-21 18:32:57 +0100

    uhhh

---
+++ api/sendEmail.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+
+app.use(express.json());
+
+app.post('/api/sendEmail', async (req, res) => {
+  const { name, email } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: 'justinmguo0@gmail.com',
+      to: email,
+      subject: 'Form Submission',
+      text: `Thank you for submitting the form with name: ${name}.`,
+    });
+
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/FormComponent.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const FormComponent = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    const formData = {
+      name,
+      email,
+    };
+  
+    try {
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (response.ok) {
+        console.log('Form data submitted successfully:', formData);
+        alert('Email sent successfully!');
+      } else {
+        console.error('Error sending email:', await response.text());
+        alert('An error occurred while sending the email.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error:', error);
+    }
+  };
+  
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FormComponent;
