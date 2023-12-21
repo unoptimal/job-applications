36291ff31029875133e5f5b284d283ad2382db71
unoptimal <unoptimalblogs@gmail.com>
2023-12-21 19:12:24 +0100

    pain

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     return (
         <div className="App">
             <h1>React Form Example</h1>
+            <h1>life is actually pain</h1>
             <FormComponent />
         </div>
     );
